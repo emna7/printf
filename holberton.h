8955d8088a6c3ba40bba2b7d1dc70224084786ea
Emna <bhmemna7@gmail.com>
2019-10-29 13:59:26 +0000

    create

--- holberton.h
+++ holberton.h
@@ -1,38 +1,29 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
-
-#include <stdlib.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
-
-#define BUFFER_SIZE 1024
-
 /**
-* struct spec - function that convert specifiers
-* @s: conversion specifier
-* @f: is a function
+* struct mystr - pointer to function with corresponding letter
+* @letter: specifier
+* @func: print argument
 */
-typedef struct spec
+typedef struct mystr
 {
-char s;
-int (*f)(va_list, char *, int *, int *, char *);
-} spec_t;
+char letter;
+int (*func)(va_list);
+} mystr;
 
+
+int _putchar(char c);
 int _printf(const char *format, ...);
-int (*get_type(char))(va_list, char *, int *, int *, char *);
-int print(char *, size_t);
-
-int get_substring_length(const char *s);
-int get_specifier_length(const char *s);
-int get_printable_length(const char *s);
-int contains(char *s, char c);
-char validate_spec(char *spec);
-void buffer_full(char *buffer, int *pos, int *n_printed);
-void string_to_buffer(char *s, char *buffer, int *pos, int *n_printed);
-int convert(va_list valist, char *buffer, int *pos, int *n_printed, char *spec);
-int _strlen(char *s);
-char *_strncpy(char *dest, const char *src, int n);
-char *_strdup(char *str);
-void rev_string(char *s);
-
-
+int print_c(va_list c);
+int print_s(va_list s);
+int(*getspecifier(char))(va_list);
+int _print_i(va_list vi);
+int print_bin(va_list bin);
+int print_rs(va_list rs);
+int print_Xhexa(va_list args);
+char *convert(unsigned int num, unsigned int size, int base);
+int print_oct(va_list oct);
+int print_u(va_list un);
+int print_rot(va_list ro);
 #endif
