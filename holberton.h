901f7127854abeb95ce21983c703090323c9f1b7
Emna <bhmemna7@gmail.com>
2019-10-29 14:47:16 +0000

    create

--- holberton.h
+++ holberton.h
@@ -1,20 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdarg.h>
-/**
-* struct mystr - pointer to function with corresponding letter
-* @letter: specifier
-* @func: print argument
-*/
-typedef struct mystr
-{
-char letter;
-int (*func)(va_list);
-} mystr;
-
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+#include <stdarg.h>
+/**
+* struct fondation - pointer to function with corresponding letter
+* @letter: specifier
+* @func: print argument
+*/
+typedef struct fondation
+{
+char letter;
+int (*func)(va_list);
+} fondation;
+
 int print_c(va_list c);
 int print_s(va_list s);
 int(*getspecifier(char))(va_list);
